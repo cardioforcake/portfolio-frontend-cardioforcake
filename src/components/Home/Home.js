38fbcff90c906008592c188037da0cc498989806
cardioforcake <chaos_241@hotmail.com>
2021-06-16 01:44:12 -0700

    minor landing page text change

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ function Home(props){
     return(
         <div className={styles.body}>
             <h3 className={styles.name}>Hi, my name is Stan</h3>
-            <h4 className={styles.intro}>Please take a look at some of the projects that I've worked on!</h4>
+            <h4 className={styles.intro}>Please take a look at my most recent project!</h4>
             <div className={styles.card}>
                 <Link to="/snake" className="main-buttons">
                     <img src="https://i.imgur.com/ip733jM.png" className={styles.img} />
